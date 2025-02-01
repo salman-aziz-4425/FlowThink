d2850265a15a77baeb142ea220dced5736b2d355
SalmanAziz <salmanaziz@Salman-Aziz.local>
2025-02-02 02:45:52 +0500

    feat: implement pycache

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 
 const LandingPage = () => {
   const navigate = useNavigate();
